553cbc0993e8cff06f780a2a0e0db72a3552d698
James-Wong <380928526@qq.com>
2018-04-11 01:35:48 +0800

    add req token

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import qs from 'qs';
-import { router } from '@/router/index';
-import { Message } from 'iview';
+import {
+    router
+} from '@/router/index';
+import {
+    Message
+} from 'iview';
 import Cookies from 'js-cookie';
 
 // 设置基础后台请求url
@@ -29,12 +33,14 @@ ajax.interceptors.request.use(
             config.data.t = new Date() * 1;
         }
         // jwt token
-        // if (true) {
-        //     config.headers.Authorization = 'bearersad'
-        // }
+        if (true) {
+            config.headers.Authorization = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.Inc3ejVwUVJEaXJQQmo4TzR4ZldaYlc4ZnpmY2hDK0VpNytQMmZyT0lTS1ppN3NHUXA1M0I0dEFRZ1hkM2tjZE14UENiL2FaMTlXQ2RoUnU4Qko4S3ptK1VxVUVxbjQzUjIvUk9ZY3pHK3ZTNC9keUpNbzVDZ3UxMm9saHlDZmJhbWt2d2xiek5zQ1J0UEdKZEFTb0ci.ooAugK7Ti7Ayzt42SLG2vGcQSkgdW7__FW4W7TLDGPk';
+        }
         return config
     },
-    err => { return Promise.reject(err); }
+    err => {
+        return Promise.reject(err);
+    }
 );
 
 // 接收拦截器
@@ -46,17 +52,23 @@ ajax.interceptors.response.use(
         if (err.response) {
             switch (err.response.status) {
                 case 401:
-                    Message.warning('登录超时，请重新登录');
+                    Message.warning(err.response.data.msg);
                     // 返回 401 清除token信息并跳转到登录页面
                     Cookies.remove('_rma');
-                    router.push({ replace: true, name: 'login' });
+                    router.push({
+                        replace: true,
+                        name: 'login'
+                    });
                     break;
                 case 500:
-                    router.push({ replace: true, path: '/500' });
+                    router.push({
+                        replace: true,
+                        path: '/500'
+                    });
                     break;
-                // case 404:
-                //     router.push({ replace: true, path: '/404' });
-                //     break;
+                    // case 404:
+                    //     router.push({ replace: true, path: '/404' });
+                    //     break;
             }
         }
         return Promise.reject(err.response.data);
