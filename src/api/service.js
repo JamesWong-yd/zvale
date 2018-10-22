216bb48b359cb6d1f9f3958b906bfca3dce3665d
几度二十 <380928526@qq.com>
2018-10-22 10:59:58 +0800

    安德森

--- src/api/service.js
+++ src/api/service.js
@@ -1,4 +1,4 @@
 // 设置基础后台请求url
-const ajaxUrl = 'http://localhost:3000/bm/';
-// const ajaxUrl = 'https://bm.zvale.com/bm/';
+// const ajaxUrl = 'http://localhost:3000/bm/';
+const ajaxUrl = 'https://bm.zvale.com/bm/';
 module.exports = ajaxUrl;
